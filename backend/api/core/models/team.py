729f53caae89efcbd9774fedec2ddad02f13b830
Dominic Merchelski <dm5441@rit.edu>
2025-02-25 14:50:06 -0500

    Add Lab Team relationship

--- backend/api/core/models/team.py
+++ backend/api/core/models/team.py
@@ -4,5 +4,7 @@ class Team(models.Model):
     name = models.CharField(max_length=255, unique=True, db_index=True)
     description = models.TextField()
 
+    lab = models.ForeignKey('core.Lab', on_delete=models.CASCADE)
+
     def __str__(self):
         return self.name
