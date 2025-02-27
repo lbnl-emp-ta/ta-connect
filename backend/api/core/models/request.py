36451549ab9f08db92f0e9f34f97f785c2955aae
Dominic Merchelski <dm5441@rit.edu>
2025-02-27 11:32:58 -0500

    Add Request crediting

--- backend/api/core/models/request.py
+++ backend/api/core/models/request.py
@@ -8,10 +8,15 @@ class Request(models.Model):
     projected_end_date = models.DateField(blank=True, null=True)
     completion_date = models.DateField(blank=True, null=True)
     statement_of_work_date = models.DateField(blank=True, null=True)
-    depth = models.ForeignKey('core.Depth', blank=True, null=True, on_delete=models.PROTECT)
 
+    depth = models.ForeignKey('core.Depth', blank=True, null=True, on_delete=models.PROTECT)
     sectors = models.ManyToManyField('core.Sector')
     topics = models.ManyToManyField('core.Topic')
+
+    # used to determine where Request is coming from and who to give completion credit to
+    crediting_program = models.ForeignKey('core.Program', on_delete=models.PROTECT)
+    crediting_lab = models.ForeignKey('core.Lab', on_delete=models.PROTECT)
+    crediting_team = models.ForeignKey('core.Team', on_delete=models.PROTECT)
 
     def __str__(self):
         return self.pk
