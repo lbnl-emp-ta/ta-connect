644f68423fea55eefce53dcf4b0b034c1af1abac
Dominic Merchelski <dm5441@rit.edu>
2025-03-04 17:10:25 -0500

    Add default RequestStatus for new Requests

--- backend/api/core/models/request.py
+++ backend/api/core/models/request.py
@@ -1,7 +1,8 @@
 from django.db import models
+from core.models import RequestStatus
 
 class Request(models.Model):
-    status = models.ForeignKey("core.RequestStatus", on_delete=models.PROTECT)
+    status = models.ForeignKey(RequestStatus, on_delete=models.PROTECT, default=RequestStatus.get_default_pk)
     description = models.TextField()
     date_created = models.DateTimeField(auto_now_add=True)
     proj_start_date = models.DateTimeField(blank=True, null=True, verbose_name="projected start date")
--- backend/api/core/models/request_status.py
+++ backend/api/core/models/request_status.py
@@ -4,6 +4,15 @@ class RequestStatus(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
 
+    @classmethod
+    def get_default_pk(cls):
+        status, _ = cls.objects.get_or_create(
+            name="New",
+            description="The default status of a new request."
+        )
+
+        return status.pk
+
     def __str__(self):
         return self.name
     
