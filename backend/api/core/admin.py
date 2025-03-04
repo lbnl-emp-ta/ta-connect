765d6c67bcd34e69bfc32c0e2455671fd6523d6e
Dominic Merchelski <dm5441@rit.edu>
2025-03-04 14:59:27 -0500

    Remove references to non-existant models

--- backend/api/core/admin.py
+++ backend/api/core/admin.py
@@ -1,8 +1,4 @@
 from django.contrib import admin
 from django.contrib.auth.models import Group
-from core.models import Request, RequestStatus 
 
 admin.site.unregister(Group)
-
-admin.site.register(Request)
-admin.site.register(RequestStatus)
