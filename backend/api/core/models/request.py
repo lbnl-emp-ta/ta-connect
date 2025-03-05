76b7a299b122ee84f1996dc839d38fcbceee57ae
Dominic Merchelski <dm5441@rit.edu>
2025-03-05 11:38:49 -0500

    Add statement of work approval date field to Request

--- backend/api/core/models/request.py
+++ backend/api/core/models/request.py
@@ -5,6 +5,7 @@ class Request(models.Model):
     status = models.ForeignKey(RequestStatus, on_delete=models.PROTECT, default=RequestStatus.get_default_pk)
     description = models.TextField()
     date_created = models.DateTimeField(auto_now_add=True)
+    sow_approval_date = models.DateTimeField(blank=True, null=True, verbose_name="statement of work approval date")
     proj_start_date = models.DateTimeField(blank=True, null=True, verbose_name="projected start date")
     proj_completion_date = models.DateTimeField(blank=True, null=True, verbose_name="projected completion date")
     actual_completion_date = models.DateTimeField(blank=True, null=True)
