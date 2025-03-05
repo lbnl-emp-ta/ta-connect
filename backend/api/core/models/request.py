edc83e29cade7953c2d2929ae9a1cd3fbe9c05ca
Dominic Merchelski <dm5441@rit.edu>
2025-03-05 13:13:37 -0500

    Add Request db constraints and tests for them

--- backend/api/core/models/request.py
+++ backend/api/core/models/request.py
@@ -1,17 +1,43 @@
+import datetime
+
 from django.db import models
+from django.db.models import CheckConstraint, Q, F
 from core.models import RequestStatus
 
 class Request(models.Model):
     status = models.ForeignKey(RequestStatus, on_delete=models.PROTECT, default=RequestStatus.get_default_pk)
     description = models.TextField()
     date_created = models.DateTimeField(auto_now_add=True)
-    sow_approval_date = models.DateTimeField(blank=True, null=True, verbose_name="statement of work approval date")
-    proj_start_date = models.DateTimeField(blank=True, null=True, verbose_name="projected start date")
-    proj_completion_date = models.DateTimeField(blank=True, null=True, verbose_name="projected completion date")
-    actual_completion_date = models.DateTimeField(blank=True, null=True)
+    sow_approval_date = models.DateField(blank=True, null=True, verbose_name="statement of work approval date")
+    proj_start_date = models.DateField(blank=True, null=True, verbose_name="projected start date")
+    proj_completion_date = models.DateField(blank=True, null=True, verbose_name="projected completion date")
+    actual_completion_date = models.DateField(blank=True, null=True)
 
     def __str__(self):
         return f"Request #{self.pk}"
+
     
     class Meta:
         db_table = "request"
+        constraints = [
+            CheckConstraint(
+                check = Q(proj_start_date__gte=datetime.date.today()) | Q(proj_start_date__isnull=True),
+                name = "projected_start_date_in_future_or_null",
+                violation_error_message="The projected start date must not be set in the past."
+            ),
+            CheckConstraint(
+                check = Q(proj_completion_date__gte=datetime.date.today()) | Q(proj_completion_date__isnull=True),
+                name = "projected_completion_date_in_future_or_null",
+                violation_error_message="The projected completion date must not be set in the past."
+            ),
+            CheckConstraint(
+                check = Q(proj_completion_date__gt=F('proj_start_date')) | Q(proj_start_date__isnull=True),
+                name = "projected_completion_date_after_projected_start_date_or_null",
+                violation_error_message="The projected completion date must be after the projected start date"
+            ),
+            CheckConstraint(
+                check = Q(proj_start_date__lt=F('proj_completion_date')) | Q(proj_completion_date__isnull=True),
+                name = "projected_start_date_before_projected_completion_date_or_null",
+                violation_error_message="The projected start date must be before the projected completion date"
+            )
+        ]
---
+++ backend/api/core/tests/test_request.py
@@ -0,0 +1,115 @@
+import datetime
+
+from django.test import TestCase
+from core.models import Request
+from django.db.utils import IntegrityError
+
+class RequestModelTests(TestCase):
+    def test_proj_start_date_in_past(self):
+        """
+        The projected start date of a Request should not be
+        in the past.
+        """
+        
+        yesterday = datetime.date.today() + datetime.timedelta(days=-1)
+        
+        with self.assertRaises(IntegrityError):
+            Request.objects.create(
+                description="test", 
+                proj_start_date=yesterday
+            )
+            
+    def test_proj_completion_date_in_past(self):
+        """
+        The projected start date of a Request should not be
+        in the past.
+        """
+        
+        yesterday = datetime.date.today() + datetime.timedelta(days=-1)
+        
+        with self.assertRaises(IntegrityError):
+            Request.objects.create(
+                description="test", 
+                proj_completion_date=yesterday
+            )
+            
+    def test_proj_start_date_after_proj_completion_date(self):
+        """
+        The projected start date should be before the projected 
+        completion date.
+        """
+        
+        completion_date = datetime.date.today()
+        start_date = completion_date + datetime.timedelta(days=1)
+        
+        request = Request(
+            description="test",
+            proj_completion_date=completion_date
+        )
+        
+        request.proj_start_date = start_date
+        
+        with self.assertRaises(IntegrityError): 
+            request.save()
+            
+    def test_proj_completion_date_before_start_date(self):
+        """
+        The projected completion date should be after the 
+        projected start date.
+        """
+        
+        completion_date = datetime.date.today()
+        start_date = completion_date + datetime.timedelta(days=1)
+        
+        request = Request(
+            description="test",
+            proj_start_date=start_date
+        )
+        
+        request.proj_completion_date = completion_date
+        
+        with self.assertRaises(IntegrityError): 
+            request.save()
+            
+            
+    def test_can_set_proj_start_date_if_proj_completion_date_null(self):
+        """
+        The projected start date can be set if any valid future date
+        if the projected completion date has not been set.
+        """
+        
+        arbitrary_future_date = datetime.date.today() + datetime.timedelta(days=7)
+        
+        did_not_raise_integrity_error = True
+        
+        try:
+            Request.objects.create(
+                description="test",
+                proj_start_date=arbitrary_future_date, 
+                proj_completion_date=None
+            )
+        except IntegrityError:
+            did_not_raise_integrity_error = False
+            
+        self.assertTrue(did_not_raise_integrity_error)
+        
+    def test_can_set_proj_completion_date_if_proj_start_date_null(self):
+        """
+        The projected start date can be set if any valid future date
+        if the projected completion date has not been set.
+        """
+        
+        arbitrary_future_date = datetime.date.today() + datetime.timedelta(days=7)
+        
+        did_not_raise_integrity_error = True
+        
+        try:
+            Request.objects.create(
+                description="test",
+                proj_start_date=None, 
+                proj_completion_date=arbitrary_future_date
+            )
+        except IntegrityError:
+            did_not_raise_integrity_error = False
+            
+        self.assertTrue(did_not_raise_integrity_error)
