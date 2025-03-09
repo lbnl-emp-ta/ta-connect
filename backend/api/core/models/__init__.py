132906fb58bc4acf43d1aaa083b4bd2ba053945d
Dominic Merchelski <dm5441@rit.edu>
2025-03-08 21:26:33 -0500

    Add Depth model class to core.models __init__

--- backend/api/core/models/__init__.py
+++ backend/api/core/models/__init__.py
@@ -1,7 +1,9 @@
 from .request_status import RequestStatus
 from .request import Request
+from .depth import Depth
 
 __all__ = [
     "RequestStatus",
-    "Request"
+    "Request",
+    "Depth",
 ]
