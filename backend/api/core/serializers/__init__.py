f9a8b96590a5d59bd7fbb1f85550fc184596b3b5
Dominic Merchelski <dm5441@rit.edu>
2025-03-09 13:56:03 -0400

    Add StateSerializer

--- backend/api/core/serializers/__init__.py
+++ backend/api/core/serializers/__init__.py
@@ -1,7 +1,9 @@
 from .request import RequestSerializer
 from .depth import DepthSerializer
+from .state import StateSerializer
 
 __all__ = [
     "RequestSerializer",
     "DepthSerializer",
+    "StateSerializer",
 ]
