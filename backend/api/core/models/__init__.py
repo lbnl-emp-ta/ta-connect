0871128b9651759ff5952c2a206f944cc6cfb799
Dominic Merchelski <dm5441@rit.edu>
2025-03-09 13:54:04 -0400

    Add State model

--- backend/api/core/models/__init__.py
+++ backend/api/core/models/__init__.py
@@ -1,9 +1,11 @@
 from .request_status import RequestStatus
 from .request import Request
 from .depth import Depth
+from .state import State
 
 __all__ = [
     "RequestStatus",
     "Request",
     "Depth",
+    "State",
 ]
