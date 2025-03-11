3795a7a1e181fa2d1b6004913120c9b92922ee64
Dominic Merchelski <dm5441@rit.edu>
2025-03-11 10:36:50 -0400

    Add Organization Serializer

--- backend/api/core/serializers/__init__.py
+++ backend/api/core/serializers/__init__.py
@@ -2,10 +2,12 @@ from .request import RequestSerializer
 from .depth import DepthSerializer
 from .state import StateSerializer
 from .organization_type import OrganizationTypeSerializer
+from .organization import OrganizationSerializer
 
 __all__ = [
     "RequestSerializer",
     "DepthSerializer",
     "StateSerializer",
     "OrganizationTypeSerializer",
+    "OrganizationSerializer",
 ]
