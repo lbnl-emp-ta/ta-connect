4de45006240f76862e1178acbaf915d8d287203d
Dominic Merchelski <dm5441@rit.edu>
2025-03-12 15:59:13 -0400

    Add Customer relation to Cohort

--- backend/api/core/models/cohort.py
+++ backend/api/core/models/cohort.py
@@ -1,11 +1,13 @@
 from django.db import models
 
-from core.models import Request
+from core.models import Request, Customer
 
 class Cohort(models.Model):
     request = models.ForeignKey(Request, on_delete=models.PROTECT)
     name = models.CharField(max_length=256)
     description = models.TextField()
+    
+    customers = models.ManyToManyField(Customer)
     
     def __str__(self):
         return self.name
