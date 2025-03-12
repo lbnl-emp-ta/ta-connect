b50c2e3e6f01cf12158643b74feccc4d583359a9
Dominic Merchelski <dm5441@rit.edu>
2025-03-12 11:34:35 -0400

    Refactor State tests

--- backend/api/core/tests/endpoints/conftest.py
+++ backend/api/core/tests/endpoints/conftest.py
@@ -1,7 +1,15 @@
 import pytest
+from pytest_django.plugin import django_db_blocker
 
 from rest_framework.test import APIClient
+
+from core.models import State
 
 @pytest.fixture(scope="module")
 def api_client():
     return APIClient()
+
+@pytest.fixture(scope="module")
+def test_state(django_db_blocker):
+    with django_db_blocker.unblock():
+        return State.objects.create(name="For Test", abbreviation="FT")
--- backend/api/core/tests/endpoints/test_state.py
+++ backend/api/core/tests/endpoints/test_state.py
@@ -1,28 +1,16 @@
 import pytest
 
-from rest_framework.test import APIClient
-
 from core.models import State
 
 @pytest.mark.django_db
 class TestStateListEndpoint():
-    @classmethod  
-    def setup_class(cls):
-        cls.client = APIClient()
-    
-    def test_list_states_endpoint_exists_at_desired_location(self):
-        response = self.client.get("/states/", follow=True)
+    def test_list_states_endpoint_exists_at_desired_location(self, api_client):
+        response = api_client.get("/states/", follow=True)
         assert response.status_code == 200
 
 
 @pytest.mark.django_db
 class TestStateRetrieveEndpoint():
-    @classmethod  
-    def setup_class(cls):
-        cls.client = APIClient()
-    
-    def test_retrieve_state_endpoint_exists_at_desired_location(self):
-        test_depth = State.objects.create(name="For Test", abbreviation="FT")
-        
-        response = self.client.get(f"/states/{test_depth.pk}", follow=True)
+    def test_retrieve_state_endpoint_exists_at_desired_location(self, api_client, test_state):
+        response = api_client.get(f"/states/{test_state.pk}", follow=True)
         assert response.status_code == 200
