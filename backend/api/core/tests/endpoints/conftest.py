6be99abc599a17d41759520faa6d69995ec6c16f
Dominic Merchelski <dm5441@rit.edu>
2025-03-12 11:39:00 -0400

    Refactor OrganizationType endpoint tests

--- backend/api/core/tests/endpoints/conftest.py
+++ backend/api/core/tests/endpoints/conftest.py
@@ -3,7 +3,7 @@ from pytest_django.plugin import django_db_blocker
 
 from rest_framework.test import APIClient
 
-from core.models import State
+from core.models import State, OrganizationType
 
 @pytest.fixture(scope="module")
 def api_client():
@@ -13,3 +13,8 @@ def api_client():
 def test_state(django_db_blocker):
     with django_db_blocker.unblock():
         return State.objects.create(name="For Test", abbreviation="FT")
+
+@pytest.fixture(scope="module")
+def test_organization_type(django_db_blocker):
+    with django_db_blocker.unblock():
+        return OrganizationType.objects.create(name="Test", description="for testing")
--- backend/api/core/tests/endpoints/test_organization_type.py
+++ backend/api/core/tests/endpoints/test_organization_type.py
@@ -6,23 +6,13 @@ from core.models import OrganizationType
 
 @pytest.mark.django_db
 class TestOrganizationTypeListEndpoint():
-    @classmethod  
-    def setup_class(cls):
-        cls.client = APIClient()
-    
-    def test_list_organization_types_endpoint_exists_at_desired_location(self):
-        response = self.client.get("/organization-types/", follow=True)
+    def test_list_organization_types_endpoint_exists_at_desired_location(self, api_client):
+        response = api_client.get("/organization-types/", follow=True)
         assert response.status_code == 200
 
 
 @pytest.mark.django_db
 class TestOrganizationTypeRetrieveEndpoint():
-    @classmethod  
-    def setup_class(cls):
-        cls.client = APIClient()
-    
-    def test_retrieve_organization_type_endpoint_exists_at_desired_location(self):
-        test_depth = OrganizationType.objects.create(name="Test", description="for testing")
-        
-        response = self.client.get(f"/organization-types/{test_depth.pk}", follow=True)
+    def test_retrieve_organization_type_endpoint_exists_at_desired_location(self, api_client, test_organization_type):
+        response = api_client.get(f"/organization-types/{test_organization_type.pk}", follow=True)
         assert response.status_code == 200
