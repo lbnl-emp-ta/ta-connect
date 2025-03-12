22700e3882aa9bdd1a812aedf05a9772a6f0bbe4
Dominic Merchelski <dm5441@rit.edu>
2025-03-12 11:36:48 -0400

    Refactor TPR endpoint test

--- backend/api/core/tests/endpoints/test_transmission_planning_region.py
+++ backend/api/core/tests/endpoints/test_transmission_planning_region.py
@@ -1,14 +1,8 @@
 import pytest
 
-from rest_framework.test import APIClient
-
 
 @pytest.mark.django_db
 class TestTransmissionPlanningRegionListEndpoint():
-    @classmethod  
-    def setup_class(cls):
-        cls.client = APIClient()
-    
-    def test_list_depths_endpoint_exists_at_desired_location(self):
-        response = self.client.get("/transmission-planning-regions/", follow=True)
+    def test_list_depths_endpoint_exists_at_desired_location(self, api_client):
+        response = api_client.get("/transmission-planning-regions/", follow=True)
         assert response.status_code == 200
