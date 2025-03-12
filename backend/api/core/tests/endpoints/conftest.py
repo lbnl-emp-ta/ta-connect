89708b88add91dbc150b5f75474ed17ddad8fbe2
Dominic Merchelski <dm5441@rit.edu>
2025-03-12 11:45:40 -0400

    Refactor Organization endpoint test

--- backend/api/core/tests/endpoints/conftest.py
+++ backend/api/core/tests/endpoints/conftest.py
@@ -1,20 +1,18 @@
 import pytest
-from pytest_django.plugin import django_db_blocker
-
 from rest_framework.test import APIClient
 
 from core.models import State, OrganizationType
 
-@pytest.fixture(scope="module")
+@pytest.fixture(scope="session")
 def api_client():
     return APIClient()
 
-@pytest.fixture(scope="module")
+@pytest.fixture(scope="session")
 def test_state(django_db_blocker):
     with django_db_blocker.unblock():
         return State.objects.create(name="For Test", abbreviation="FT")
 
-@pytest.fixture(scope="module")
+@pytest.fixture(scope="session")
 def test_organization_type(django_db_blocker):
     with django_db_blocker.unblock():
-        return OrganizationType.objects.create(name="Test", description="for testing")
+        return OrganizationType.objects.create(name="TestOrgType", description="for testing")
--- backend/api/core/tests/endpoints/test_organization.py
+++ backend/api/core/tests/endpoints/test_organization.py
@@ -7,36 +7,19 @@ from core.models import OrganizationType
 
 @pytest.mark.django_db
 class TestOrganizationListEndpoint():
-    @classmethod  
-    def setup_class(cls):
-        cls.client = APIClient()
-    
-    def test_list_organization_endpoint_exists_at_desired_location(self):
-        response = self.client.get("/organizations/")
+    def test_list_organization_endpoint_exists_at_desired_location(self, api_client):
+        response = api_client.get("/organizations/")
         assert response.status_code == 200
 
 @pytest.mark.django_db
 class TestOrganizationCreateEndpoint():
-    @classmethod  
-    def setup_class(cls):
-        cls.client = APIClient()
-        
-    @classmethod
-    def make_post_request_with(cls, data):
-        return cls.client.post(reverse("organization-create"), data=data)
-    
-    def test_create_request_endpoint_exists_at_desired_location(self):
-        test_org = OrganizationType.objects.create(
-            name="TestOrgType",
-            description="For testing purposes."
-        )
-        
+    def test_create_request_endpoint_exists_at_desired_location(self, api_client, test_organization_type):
         data = {
             "name": "TestOrg",
             "address": "123 Test Street, TestVille, South Testerland",
-            "type": test_org.pk,
+            "type": test_organization_type.pk,
         }
         
-        response = self.client.post("/organizations/", data=data)
+        response = api_client.post("/organizations/", data=data)
         assert response.status_code == 201
     
