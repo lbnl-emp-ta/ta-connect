c215b33822b3d42e1b3fe9d487c36b123f882883
Dominic Merchelski <dm5441@rit.edu>
2025-03-27 11:53:14 -0400

    Initial TanStack Router setup

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,8 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import ReactDOM from 'react-dom/client'
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { RouterProvider, createRouter } from '@tanstack/react-router'
+
 import './index.css'
 import App from './App.tsx'
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
@@ -11,10 +14,28 @@ import '@fontsource/roboto/700.css';
 
 const queryClient = new QueryClient(); 
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <QueryClientProvider client={queryClient}>
-        <App />
-    </QueryClientProvider>
-  </StrictMode>,
-)
+// Import the generated route tree
+import { routeTree } from './routeTree.gen'
+
+// Create a new router instance
+const router = createRouter({ routeTree })
+
+// Register the router instance for type safety
+declare module '@tanstack/react-router' {
+    interface Register {
+        router: typeof router
+    }
+}
+
+// Render the app
+const rootElement = document.getElementById('root')!
+if (!rootElement.innerHTML) {
+    const root = ReactDOM.createRoot(rootElement)
+    root.render(
+    <StrictMode>
+        <QueryClientProvider client={queryClient}>
+            <RouterProvider router={router} />
+        </QueryClientProvider>
+    </StrictMode>,
+  )
+}
