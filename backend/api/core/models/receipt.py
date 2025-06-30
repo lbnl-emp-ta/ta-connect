6bf85e786707d965d2fb5d61e8b03b5a02b33b0c
Dominic Merchelski <dm5441@rit.edu>
2025-06-30 13:10:01 -0400

    add comment claryfing receipt model's purpose

--- backend/api/core/models/receipt.py
+++ backend/api/core/models/receipt.py
@@ -2,6 +2,12 @@ from django.db import models
 
 from core.models import *
 
+# Used to orient a request within a program. Without this information a 
+# request would be ambiguous. For example:
+#
+# A request that is owned by a lab participating in two or more programs (A and B) would be ambiguous. 
+# Is the request downstream of Program A, or Program B? There would be no way of knowing to 
+# which program the request belongs.
 class Receipt(models.Model):
     program = models.ForeignKey(Program, blank=True, null=True, on_delete=models.PROTECT)
     lab = models.ForeignKey(Lab, blank=True, null=True, on_delete=models.PROTECT)
