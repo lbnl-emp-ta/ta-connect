de3217be8b3c3e780594bbb28443a0c05b1af73f
Dominic Merchelski <dm5441@rit.edu>
2025-07-03 15:46:42 -0400

    Add more nested info in request detail

--- backend/api/core/serializers/__init__.py
+++ backend/api/core/serializers/__init__.py
@@ -1,4 +1,8 @@
 from .user import UserLeanSerializer
+from .state import StateSerializer
+from .transmission_planning_region import TransmissionPlanningRegionSerializer
+from .organization_type import OrganizationTypeSerializer
+from .organization import OrganizationSerializer
 from .customer import CustomerSerializer
 from .depth import DepthSerializer
 from .lab import LabSerializer
@@ -8,10 +12,6 @@ from .role import RoleSerializer
 from .request_status import RequestStatusSerializer
 from .owner import OwnerSerializer
 from .request import RequestSerializer, RequestListSerializer, RequestDetailSerializer
-from .state import StateSerializer
-from .organization_type import OrganizationTypeSerializer
-from .organization import OrganizationSerializer
-from .transmission_planning_region import TransmissionPlanningRegionSerializer
 from .customer_type import CustomerTypeSerializer
 from .customer_request_relationship import CustomerRequestRelationshipSerializer
 from .cohort import CohortSerializer
--- backend/api/core/serializers/organization.py
+++ backend/api/core/serializers/organization.py
@@ -1,8 +1,10 @@
 from rest_framework import serializers
 
 from core.models import Organization
+from core.serializers import *
 
 class OrganizationSerializer(serializers.ModelSerializer):
+    type = OrganizationTypeSerializer()
     class Meta:
         model = Organization
         fields = "__all__"
