66cf570342d48000652edda15a3ad47badcff8c7
Cody O'Donnell <ctodonnell@lbl.gov>
2025-07-22 11:28:05 -0700

    Fix request editing so that only edited fields are sent to the API

--- frontend/src/features/requests/RequestInfoPanel.tsx
+++ frontend/src/features/requests/RequestInfoPanel.tsx
@@ -22,7 +22,7 @@ import { DatePicker } from '@mui/x-date-pickers';
 import { PickerValue } from '@mui/x-date-pickers/internals';
 import dayjs, { Dayjs } from 'dayjs';
 import { useCallback, useEffect, useState } from 'react';
-import { TARequestDetail } from '../../api/dashboard/types';
+import { TARequest, TARequestDetail } from '../../api/dashboard/types';
 import { InfoPanel } from '../../components/InfoPanel';
 import { useRequestMutation } from '../../utils/queryOptions';
 import { capitalize, formatDatetime } from '../../utils/utils';
@@ -68,14 +68,38 @@ export const RequestInfoPanel: React.FC<RequestInfoPanelProps> = ({ request }) =
     setEditing(true);
   };
 
+  /**
+   * Handle submission of edited request information.
+   * Only send fields that have changed to the API.
+   * If a field is set explicitly to null, it will be cleared in the API.
+   */
   const handleEditSubmit = () => {
-    updateRequestMutation.mutate({
-      depth,
-      proj_start_date: projectedStartDate?.format('YYYY-MM-DD') || null,
-      proj_completion_date: projectedCompletionDate?.format('YYYY-MM-DD') || null,
-      actual_completion_date: actualCompletionDate?.format('YYYY-MM-DD') || null,
-      description,
-    });
+    const mutationData = {} as Partial<TARequest>;
+    if (depth !== request?.depth) {
+      mutationData.depth = depth;
+    }
+    if (description !== request?.description) {
+      mutationData.description = description;
+    }
+    if (projectedStartDate === null) {
+      mutationData.proj_start_date = null;
+    }
+    if (projectedStartDate?.format('YYYY-MM-DD') !== request?.proj_start_date) {
+      mutationData.proj_start_date = projectedStartDate?.format('YYYY-MM-DD');
+    }
+    if (projectedCompletionDate === null) {
+      mutationData.proj_completion_date = null;
+    }
+    if (projectedCompletionDate?.format('YYYY-MM-DD') !== request?.proj_completion_date) {
+      mutationData.proj_completion_date = projectedCompletionDate?.format('YYYY-MM-DD');
+    }
+    if (actualCompletionDate === null) {
+      mutationData.actual_completion_date = null;
+    }
+    if (actualCompletionDate?.format('YYYY-MM-DD') !== request?.actual_completion_date) {
+      mutationData.actual_completion_date = actualCompletionDate?.format('YYYY-MM-DD');
+    }
+    updateRequestMutation.mutate(mutationData);
   };
 
   const handleEditCancel = () => {
