c3bef87238ec27dd779526c2428f6c925b9933d2
Dominic Merchelski <dm5441@rit.edu>
2025-07-28 14:57:14 -0400

    initial add of topics to patch request

--- backend/api/core/views/request.py
+++ backend/api/core/views/request.py
@@ -333,7 +333,27 @@ class RequestDetailView(BaseUserAwareRequest):
                 return Response(data={"message": "Provided status does not exist."}, status=status.HTTP_400_BAD_REQUEST)
 
             patch_data["status"] = maybe_status.name 
-    
+        
+        # Topics are done a special way (not using patch serizlier) because they are 
+        # stored as a Many-to-Many relationship in the database.
+        if "topics" in body:
+            if body.get("topics") is None:
+                maybe_request.topics.clear()
+            
+            current_topics = maybe_request.topics.all()
+            maybe_request.topics.clear()
+            
+            topics = body.get("topics")
+            for topic_name in topics:
+                try:
+                    topic = Topic.objects.get(name=topic_name)
+                except Topic.DoesNotExist:
+                    maybe_request.topics.clear()
+                    maybe_request.topics.add(current_topics)
+                    return Response(data={"message": "One of the provided topics does not exist."}, status=status.HTTP_400_BAD_REQUEST)
+
+                maybe_request.topics.add(topic)
+                
         
        # do partial save with accumulated patch 
         patch_serializer = RequestSerializer(instance=maybe_request, data=patch_data, partial=True)
