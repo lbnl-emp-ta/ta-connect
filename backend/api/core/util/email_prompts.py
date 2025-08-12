fcc62dd83966d7ad5f8c7ed053d55a10a6768828
Dominic Merchelski <dm5441@rit.edu>
2025-08-12 12:49:19 -0400

    improve generic email prompt and update assignment notification

--- backend/api/core/util/email_prompts.py
+++ backend/api/core/util/email_prompts.py
@@ -1,4 +1,3 @@
-def generic_template(message: str) -> str:
-    template = f"""Hello,\n\n{message}\n\nThank you,\nTACONNECT"""
-    
+def generic_template(receipient_name: str, message: str) -> str:
+    template = f"""Hello {receipient_name},\n\n{message}\n\nThank you,\nTACONNECT"""
     return template
