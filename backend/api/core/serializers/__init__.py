ff944d1bc053428c1a04d26f0da058fa06da375c
Cody O'Donnell <ctodonnell@lbl.gov>
2025-08-15 14:18:45 -0700

    Fix issues with migrations and starting up

--- backend/api/core/serializers/__init__.py
+++ backend/api/core/serializers/__init__.py
@@ -12,11 +12,10 @@ from .reception import ReceptionSerializer
 from .role import RoleSerializer
 from .request_status import RequestStatusSerializer
 from .owner import OwnerSerializer
-from .request import RequestSerializer, RequestListSerializer, RequestDetailSerializer, RequestExpertListSerializer
 from .topic import TopicSerializer
+from .request import RequestSerializer, RequestListSerializer, RequestDetailSerializer, RequestExpertListSerializer
 from .customer_request_relationship import CustomerRequestRelationshipSerializer
 from .cohort import CohortSerializer
-from .topic import TopicSerializer
 from .attachment import AttachmentUploadSerializer, AttachmentEditSerializer, AttachmentSerializer
 from .note import NoteSerializer, NoteCreateSerializer
 
