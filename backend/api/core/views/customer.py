e7dc687ee3f85b9f0684e32a24555d1be50f478f
Dominic Merchelski <dm5441@rit.edu>
2025-08-18 15:13:00 -0400

    remove code associated with culled endpoints

--- backend/api/core/views/customer.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import generics
-
-from core.models import Customer
-from core.serializers import CustomerSerializer
-
-class CustomerCreateView(generics.CreateAPIView):
-    queryset = Customer.objects.all()
-    serializer_class = CustomerSerializer
--- backend/api/core/views/depth.py
+++ backend/api/core/views/depth.py
@@ -1,12 +1,8 @@
-from rest_framework.generics import ListAPIView, RetrieveAPIView
+from rest_framework.generics import ListAPIView
 from core.models import Depth
 from core.serializers import DepthSerializer
 
 
 class DepthListView(ListAPIView):
-    queryset = Depth.objects.all()
-    serializer_class = DepthSerializer
-    
-class DepthRetrieveView(RetrieveAPIView):
     queryset = Depth.objects.all()
     serializer_class = DepthSerializer
