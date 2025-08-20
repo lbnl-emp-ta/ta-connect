71319e5c3454a45bf0fe4b42c5d83957b758ff82
Dominic Merchelski <dm5441@rit.edu>
2025-08-20 11:53:10 -0400

    fix tests and remove dead ones

--- backend/api/core/tests/conftest.py
+++ backend/api/core/tests/conftest.py
@@ -8,8 +8,6 @@ from core.models import *
 from core.fixtures.populate_db import fixture_list 
 from core.constants import ROLE
 
-TEST_USER_PASSWORD = "Password123?"
-
 
 @pytest.fixture(scope="session")
 def django_db_setup(django_db_setup, django_db_blocker):
@@ -22,17 +20,9 @@ def django_db_setup(django_db_setup, django_db_blocker):
 def api_client():
     return APIClient()
 
-# @pytest.fixture(scope="function")
-# def test_user(django_db_blocker, db_setup):
-#     with django_db_blocker.unblock():
-#         _test_user, _ = User.objects.get_or_create_user(
-#             email="testuser@gmail.com",
-#             password=TEST_USER_PASSWORD
-#         )
-
-#         print(Role.objects.all())
-        
-#         return _test_user
+@pytest.fixture(scope="function")
+def test_user(django_db_setup):
+        return User.objects.get(pk=1)
     
 
 # @pytest.fixture(scope="function")
@@ -64,14 +54,10 @@ def api_client():
 #         )
 #         return _test_org
     
-# @pytest.fixture(scope="function")   
-# def test_depth(django_db_blocker):
-#     with django_db_blocker.unblock():
-#         _test_depth, _ = Depth.objects.get_or_create(
-#             name="FixtureTestDepth", 
-#             description="for testing"
-#         )
-#         return _test_depth
+@pytest.fixture(scope="function")   
+def test_depth(django_db_blocker):
+    with django_db_blocker.unblock():
+        return Depth.objects.get(pk=1) 
         
         
 # @pytest.fixture(scope="function")   
@@ -82,20 +68,9 @@ def api_client():
 #         )
 #         return _test_tpr
     
-# @pytest.fixture(scope="function")
-# def test_customer(django_db_blocker, test_org, test_state, test_tpr):
-#     with django_db_blocker.unblock():
-#         _test_customer, _ = Customer.objects.get_or_create(
-#             org=test_org,
-#             state=test_state,
-#             tpr=test_tpr,
-#             email="fixturetestcustomer@email.com",
-#             name="FixtureTestCustomer",
-#             phone="999-999-9999",
-#             title="Tester"
-#         )
-        
-#         return _test_customer
+@pytest.fixture(scope="function")
+def test_customer(django_db_setup):
+    return Customer.objects.get(pk=1) 
     
 # @pytest.fixture(scope="function")
 # def test_request_status(django_db_blocker):
@@ -107,27 +82,14 @@ def api_client():
         
 #         return _test_request_status
 
-# @pytest.fixture(scope="function")
-# def test_request(django_db_blocker, test_request_status, test_depth):
-#     with django_db_blocker.unblock():
-#         _test_request, _ = Request.objects.get_or_create(
-#             status=test_request_status,
-#             description="for testing",
-#             depth=test_depth
-#         )
-        
-#         return _test_request
+@pytest.fixture(scope="function")
+def test_request(django_db_setup):
+    return Request.objects.get(pk=1) 
     
     
-# @pytest.fixture(scope="function")
-# def test_customer_type(django_db_blocker):
-#     with django_db_blocker.unblock():
-#         _test_customer_type, _ = CustomerType.objects.get_or_create(
-#             name="FixtureTestCustomerType",
-#             description="for testing"
-#         )
-        
-#         return _test_customer_type
+@pytest.fixture(scope="function")
+def test_customer_type(django_db_setup):
+    return CustomerType.objects.get(pk=1) 
     
 # @pytest.fixture(scope="function")
 # def test_cohort(django_db_blocker, test_request):
--- backend/api/core/tests/endpoints/test_customer_request_relationship.py
+++ backend/api/core/tests/endpoints/test_customer_request_relationship.py
@@ -8,5 +8,5 @@ class TestCustomerRequestRelationshipEndpoint():
     def test_customer_request_relationship_endpoint_exists_at_desired_location(self, api_client):
         # need to be authenticated to access endpoint
         api_client.force_login(User.objects.get(pk=1)) 
-        response = api_client.get("/api/customer-request-relationships/")
+        response = api_client.get("/api/customer-request-relationships/", headers={"Context": '{"user":1,"role":1,"location":"System"}'})
         assert response.status_code == status.HTTP_200_OK
