11e3764a9ba4ec4c9bed8529087d4975f71afe02
Dominic Merchelski <dm5441@rit.edu>
2025-08-22 16:50:58 -0400

    add test comment

--- backend/api/core/tests/conftest.py
+++ backend/api/core/tests/conftest.py
@@ -1,3 +1,4 @@
+# Define test wide dependencies to reduce repetition
 
 import pytest
 from rest_framework.test import APIClient
@@ -6,7 +7,6 @@ from django.core.management import call_command
 
 from core.models import *
 from core.fixtures.populate_db import fixture_list 
-from core.constants import ROLE
 
 
 @pytest.fixture(scope="session")
@@ -14,7 +14,6 @@ def django_db_setup(django_db_setup, django_db_blocker):
     with django_db_blocker.unblock():
         for fixture in fixture_list:
             call_command("loaddata", fixture)
-
 
 @pytest.fixture(scope="session")
 def api_client():
@@ -24,63 +23,15 @@ def api_client():
 def test_user(django_db_setup):
         return User.objects.get(pk=1)
     
-
-# @pytest.fixture(scope="function")
-# def test_state(django_db_blocker):
-#     with django_db_blocker.unblock():
-#         _test_state, _ = State.objects.get_or_create(
-#             name="FixtureTestState", 
-#             abbreviation="FTS"
-#         )
-        
-#         return _test_state
-
-# @pytest.fixture(scope="function")
-# def test_org_type(django_db_blocker):
-#     with django_db_blocker.unblock():
-#         _test_org_type, _ = OrganizationType.objects.get_or_create(
-#             name="FixtureTestOrgType", 
-#             description="for testing"
-#         )
-#         return _test_org_type
-        
-# @pytest.fixture(scope="function")   
-# def test_org(django_db_blocker, test_org_type):
-#     with django_db_blocker.unblock(): 
-#         _test_org, _ = Organization.objects.get_or_create(
-#             name="FixtureTestOrg",
-#             address="123 Test Street",
-#             type=test_org_type,
-#         )
-#         return _test_org
-    
 @pytest.fixture(scope="function")   
 def test_depth(django_db_blocker):
     with django_db_blocker.unblock():
         return Depth.objects.get(pk=1) 
         
-        
-# @pytest.fixture(scope="function")   
-# def test_tpr(django_db_blocker):
-#     with django_db_blocker.unblock():
-#         _test_tpr, _ = TransmissionPlanningRegion.objects.get_or_create(
-#             name="FixtureTestTPR", 
-#         )
-#         return _test_tpr
-    
 @pytest.fixture(scope="function")
 def test_customer(django_db_setup):
     return Customer.objects.get(pk=1) 
     
-# @pytest.fixture(scope="function")
-# def test_request_status(django_db_blocker):
-#     with django_db_blocker.unblock():
-#         _test_request_status, _ = RequestStatus.objects.get_or_create(
-#             name="FixtureTestRequestStatus",
-#             description="for testing"
-#         )
-        
-#         return _test_request_status
 
 @pytest.fixture(scope="function")
 def test_request(django_db_setup):
@@ -91,13 +42,3 @@ def test_request(django_db_setup):
 def test_customer_type(django_db_setup):
     return CustomerType.objects.get(pk=1) 
     
-# @pytest.fixture(scope="function")
-# def test_cohort(django_db_blocker, test_request):
-#     with django_db_blocker.unblock():
-#         _test_cohort, _ = Cohort.objects.get_or_create(
-#             request=test_request,
-#             name="FixtureTestCohort",
-#             description="for testing"
-#         )
-        
-#         return _test_cohort
