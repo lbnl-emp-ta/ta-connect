eb0cf58ff63a7cb31ad54b50537d21bcf1a09a04
Cody O'Donnell <ctodonnell@lbl.gov>
2026-03-31 09:06:04 -0700

    Update request stepper logic and display

--- frontend/src/features/requests/RequestStepper.tsx
+++ frontend/src/features/requests/RequestStepper.tsx
@@ -1,10 +1,16 @@
 import { TARequestDetail } from '@/api/dashboard/types';
-import { Step, StepLabel, Stepper } from '@mui/material';
+import { Chip, Stack, Step, StepLabel, Stepper, Typography } from '@mui/material';
 
 interface RequestStepperProps {
   request?: TARequestDetail;
 }
 
+/**
+ * Stepper to show the progress of a request through the system.
+ * This is different than the status of the request, which is more granular and can be updated by users.
+ * The stepper is meant to show the progression of the request through the system,
+ * and is determined by the owner and status of the request.
+ */
 export const RequestStepper: React.FC<RequestStepperProps> = ({ request }) => {
   const steps = ['Opened', 'Reception', 'Program', 'Lab', 'Expert', 'Approval', 'Completed'];
 
@@ -19,16 +25,32 @@ export const RequestStepper: React.FC<RequestStepperProps> = ({ request }) => {
       return 2;
     } else if (request.owner?.domain_type === 'lab' && request.expert === null) {
       return 3;
-    } else if (request.owner?.domain_type === 'lab' && request.expert) {
-      return 2;
+    } else if (request.owner?.domain_type === 'expert') {
+      return 4;
+    } else if (request.owner?.domain_type === 'lab' && request.expert !== null) {
+      return 5;
+    } else if (request.owner?.domain_type === 'program' && request.expert !== null) {
+      return 5;
+    } else if (
+      !request.owner &&
+      (request.status === 'Completed' || request.status === 'Unable to address')
+    ) {
+      return 6;
     }
   };
 
   return (
     <Stepper activeStep={getActiveStep()} alternativeLabel>
-      {steps.map((label) => (
+      {steps.map((label, i) => (
         <Step key={label}>
-          <StepLabel>{label}</StepLabel>
+          <StepLabel>
+            <Stack spacing={0}>
+              <Typography variant="overline" fontWeight="bold">
+                {label}
+              </Typography>
+              {getActiveStep() === i && <Chip label={request?.status} size="small" />}
+            </Stack>
+          </StepLabel>
         </Step>
       ))}
     </Stepper>
