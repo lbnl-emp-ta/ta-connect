2ecae14e5d63b69555c7d27246391417f5e8cf66
Cody O'Donnell <ctodonnell@lbl.gov>
2026-04-03 12:40:41 -0700

    Modify request tabs

--- frontend/src/features/requests/RequestsLayout.tsx
+++ frontend/src/features/requests/RequestsLayout.tsx
@@ -1,12 +1,10 @@
 import { TARequest } from '@/api/dashboard/types';
-import { AppLink } from '@/components/AppLink';
 import { useIdentityContext } from '@/features/identity/IdentityContext';
 import { useRequestsContext } from '@/features/requests/RequestsContext';
 import { RequestsList } from '@/features/requests/RequestsList';
 import { a11yProps } from '@/utils/utils';
 import SortIcon from '@mui/icons-material/Sort';
 import {
-  Badge,
   Box,
   Container,
   InputAdornment,
@@ -17,7 +15,7 @@ import {
   Tabs,
   Typography,
 } from '@mui/material';
-import { Outlet } from '@tanstack/react-router';
+import { Outlet, useNavigate } from '@tanstack/react-router';
 import { useEffect } from 'react';
 
 interface RequestsLayoutProps {
@@ -25,6 +23,7 @@ interface RequestsLayoutProps {
 }
 
 export const RequestsLayout: React.FC<RequestsLayoutProps> = ({ requestsList }) => {
+  const navigate = useNavigate();
   const { identity, detailedIdentity, setIdentity, setDetailedIdentity } = useIdentityContext();
   const { tab, sortField, setSortField, setSortedRequests } = useRequestsContext();
 
@@ -44,83 +43,60 @@ export const RequestsLayout: React.FC<RequestsLayoutProps> = ({ requestsList }) 
         }}
       >
         {requestsList && (
-          <>
-            <Stack direction="row" sx={{ marginBottom: 2 }}>
-              <Tabs
-                value={tab}
-                aria-label="requests tabs"
+          <Stack>
+            <Tabs
+              value={tab}
+              variant="fullWidth"
+              textColor="secondary"
+              indicatorColor="secondary"
+              aria-label="requests tabs"
+            >
+              <Tab
+                label="Active"
+                component="a"
+                onClick={() => navigate({ to: '/requests/active' })}
+                disableRipple
+                value="active"
                 sx={{
-                  backgroundColor: 'grey.400',
-                  borderRadius: 1,
-                  display: 'inline-flex',
-                  padding: 0.5,
-                  '& .MuiButtonBase-root': {
-                    borderRadius: 1,
+                  '&.MuiButtonBase-root': {
+                    paddingLeft: 0,
+                    paddingRight: 0,
                     textTransform: 'none',
                   },
-                  '& .MuiTab-root.Mui-selected': {
-                    backgroundColor: 'white',
-                  },
-                  '& .MuiTabs-indicator': {
-                    height: 0,
+                }}
+                {...a11yProps('active')}
+              />
+              <Tab
+                label="Inactive"
+                component="a"
+                onClick={() => navigate({ to: '/requests/inactive' })}
+                disableRipple
+                value="inactive"
+                sx={{
+                  '&.MuiButtonBase-root': {
+                    paddingLeft: 0,
+                    paddingRight: 0,
+                    textTransform: 'none',
                   },
                 }}
-              >
-                <Tab
-                  label={
-                    <AppLink
-                      to="/requests/active"
-                      sx={{
-                        color: 'inherit',
-                        transition: '0.25s',
-                        '&:hover': { color: 'secondary.dark' },
-                      }}
-                    >
-                      <Stack direction="row" alignItems="center" spacing={2}>
-                        <Typography>Active</Typography>
-                        <Badge badgeContent={requestsList.length.toString()} color="primary" />
-                      </Stack>
-                    </AppLink>
-                  }
-                  value="active"
-                  {...a11yProps('active')}
-                />
-                <Tab
-                  label={
-                    <AppLink
-                      to="/requests/inactive"
-                      sx={{
-                        color: 'inherit',
-                        transition: '0.25s',
-                        '&:hover': { color: 'secondary.dark' },
-                      }}
-                    >
-                      <Stack direction="row" alignItems="center" spacing={2}>
-                        <Typography>Inactive</Typography>
-                        <Badge badgeContent={requestsList.length.toString()} color="primary" />
-                      </Stack>
-                    </AppLink>
-                  }
-                  value="inactive"
-                  {...a11yProps('inactive')}
-                />
-              </Tabs>
-              <Select
-                value={sortField}
-                startAdornment={
-                  <InputAdornment position="start">
-                    <SortIcon />
-                  </InputAdornment>
-                }
-                onChange={(e) => setSortField(e.target.value)}
-              >
-                <MenuItem value="-date_created">Newest first</MenuItem>
-                <MenuItem value="date_created">Oldest first</MenuItem>
-                <MenuItem value="status">Status</MenuItem>
-              </Select>
-            </Stack>
+                {...a11yProps('inactive')}
+              />
+            </Tabs>
+            <Select
+              value={sortField}
+              startAdornment={
+                <InputAdornment position="start">
+                  <SortIcon />
+                </InputAdornment>
+              }
+              onChange={(e) => setSortField(e.target.value)}
+            >
+              <MenuItem value="-date_created">Newest first</MenuItem>
+              <MenuItem value="date_created">Oldest first</MenuItem>
+              <MenuItem value="status">Status</MenuItem>
+            </Select>
             <RequestsList requests={requestsList} />
-          </>
+          </Stack>
         )}
         {!requestsList && <Typography variant="body1">Failed to load requests.</Typography>}
       </Box>
