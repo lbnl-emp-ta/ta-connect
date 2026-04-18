a7678fcf9f1883e7eb5e3ea5776641ed731a0e12
Cody O'Donnell <ctodonnell@lbl.gov>
2026-04-17 18:52:16 -0700

    Adjust global shadows

--- frontend/src/theme.tsx
+++ frontend/src/theme.tsx
@@ -1,4 +1,4 @@
-import { createTheme } from '@mui/material';
+import { createTheme, Shadows } from '@mui/material';
 import type {} from '@mui/x-data-grid/themeAugmentation';
 
 /**
@@ -64,6 +64,25 @@ export const theme = createTheme({
     fontWeightMedium: 500,
     fontWeightBold: 700,
   },
+  shadows: [
+    'none',
+    '0 1px 2px 0 rgb(0, 0, 0, 0.14)',
+    '0 2px 4px -1px rgb(0, 0, 0, 0.14), 0 4px 5px 0 rgb(0, 0, 0, 0.12)',
+    '0 3px 5px -1px rgb(0, 0, 0, 0.14), 0 6px 10px 0 rgb(0, 0, 0, 0.12)',
+    '0 4px 5px -2px rgb(0, 0, 0, 0.14), 0 8px 10px 1px rgb(0, 0, 0, 0.12)',
+    '0 5px 5px -3px rgb(0, 0, 0, 0.14), 0 10px 14px 1px rgb(0, 0, 0, 0.12)',
+    '0 6px 6px -3px rgb(0, 0, 0, 0.14), 0 12px 17px 2px rgb(0, 0, 0, 0.12)',
+    '0 7px 8px -4px rgb(0, 0, 0, 0.14), 0 14px 21px 2px rgb(0, 0, 0, 0.12)',
+    '0 8px 9px -5px rgb(0, 0, 0, 0.14), 0 16px 24px 2px rgb(0, 0, 0, 0.12)',
+    '0 9px 10px -5px rgb(0, 0, 0, 0.14), 0 18px 28px 2px rgb(0, 0, 0, 0.12)',
+    '0 10px 11px -6px rgb(0, 0, 0, 0.14), 0 20px 31px 3px rgb(0, 0, 0, 0.12)',
+    '0 11px 15px -7px rgb(0, 0, 0, 0.14), 0 22px 38px 3px rgb(0, 0, 0, 0.12)',
+    '0 12px 17px -7px rgb(0, 0, 0, 0.14), 0 24px 40px 3px rgb(0, 0, 0, 0.12)',
+    '0 13px 19px -8px rgb(0, 0, 0, 0.14), 0 26px 44px 4px rgb(0, 0, 0, 0.12)',
+    '0 14px 21px -8px rgb(0, 0, 0, 0.14), 0 28px 45px 4px rgb(0, 0, 0, 0.12)',
+    '0 15px 22px -9px rgb(0, 0, 0, 0.14), 0 30px 46px 4px rgb(0, 0, 0, 0.12)',
+    ...Array(9).fill('0 15px 22px -9px rgb(0, 0, 0, 0.14), 0 30px 46px 4px rgb(0, 0, 0, 0.12)'),
+  ] as Shadows,
   // Default options for MUI components used throughout the app
   components: {
     /**
