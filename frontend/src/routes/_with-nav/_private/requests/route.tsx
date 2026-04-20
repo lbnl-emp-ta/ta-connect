575cf5387e29d1a51dc3893f1565ce9271c5dede
Cody O'Donnell <ctodonnell@lbl.gov>
2026-04-20 13:32:51 -0700

    Fix redirect to profile page when no roles exist for user

--- frontend/src/routes/_with-nav/_private/requests/route.tsx
+++ frontend/src/routes/_with-nav/_private/requests/route.tsx
@@ -1,11 +1,23 @@
+import { identitiesQueryOptions } from '@/utils/queryOptions';
 import { Box } from '@mui/material';
-import { createFileRoute, Outlet } from '@tanstack/react-router';
+import { useSuspenseQuery } from '@tanstack/react-query';
+import { createFileRoute, Navigate, Outlet } from '@tanstack/react-router';
 
 export const Route = createFileRoute('/_with-nav/_private/requests')({
+  loader: async ({ context }) => {
+    await context.queryClient.ensureQueryData(identitiesQueryOptions());
+  },
   component: RouteComponent,
 });
 
 function RouteComponent() {
+  const { data: identities } = useSuspenseQuery(identitiesQueryOptions());
+
+  // If the user has no identities, they shouldn't access any requests pages
+  if (!identities || identities.length === 0) {
+    return <Navigate to="/profile" />;
+  }
+
   return (
     <Box component="main" sx={{ flex: 1, overflow: 'hidden' }}>
       <Outlet />
