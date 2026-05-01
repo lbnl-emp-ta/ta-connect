9689580c7eef9811c317c22ed3bc5147bd8f3cb5
Cody O'Donnell <ctodonnell@lbl.gov>
2026-05-01 10:29:49 -0700

    Enfore program connection with lab role assignments and expertises

--- backend/api/core/models/expertise.py
+++ backend/api/core/models/expertise.py
@@ -1,12 +1,22 @@
+from django.core.exceptions import ValidationError
 from django.db import models
 from core.models import *
 
-# After a user is assigned the Expert role, they must also have various
-# expertise identified via this model to be properly filtered on the experts page.
+# After a user is assigned an Expert role, they must also have various
+# expertises identified via this model to be properly filtered in expert views.
 class Expertise(models.Model):
     user = models.ForeignKey(User, on_delete=models.PROTECT)
+    lab_role_assignment = models.ForeignKey(LabRoleAssignment, on_delete=models.PROTECT)
     topic = models.ForeignKey(Topic, on_delete=models.PROTECT)
     depth = models.ForeignKey(Depth, on_delete=models.PROTECT)
+
+    def clean(self):
+        if self.user_id != self.lab_role_assignment.user_id:
+            raise ValidationError("Expertise user must match the lab role assignment user.")
+
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return f"{self.user.email} | {self.topic.name} - {self.depth.name}"
