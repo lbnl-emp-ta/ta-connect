6a2b224cf020392dc12880c3aa42bbf6fbee2d23
Cody O'Donnell <ctodonnell@lbl.gov>
2026-06-16 12:59:08 -0700

    Add and fix intake tests

--- backend/api/core/tests/endpoints/test_intake_form.py
+++ backend/api/core/tests/endpoints/test_intake_form.py
@@ -3,9 +3,23 @@ from rest_framework import status
 
 from core.models import Customer, Request
 
-@pytest.mark.django_db(transaction=True)
+@pytest.mark.django_db
 class TestProcessIntakeFormEndpoint:
-    def test_process_intake_form_endpoint_exists_at_given_location(self, api_client, test_customer, test_request):
+    def test_process_intake_form_with_existing_organization(self, api_client, test_customer, test_request):
+        org = test_request.organization
+        data = {
+            "name": test_customer.name,
+            "email": test_customer.email,
+            "phone": test_customer.phone,
+            "title": test_customer.title,
+            "organization_id": org.id,
+            "taDepth": test_request.depth.name,
+            "description": test_request.description
+        }
+        response = api_client.post("/api/process-intake-form/", data=data)
+        assert response.status_code == status.HTTP_201_CREATED
+
+    def test_process_intake_form_with_new_organization(self, api_client, test_customer, test_request):
         org = test_request.organization
         data = {
             "name": test_customer.name,
@@ -14,11 +28,11 @@ class TestProcessIntakeFormEndpoint:
             "title": test_customer.title,
             "tpr": org.transmission_planning_region.name,
             "state": org.state.abbreviation,
-            "organization": org.name,
-            "organizationAddress": org.address,
-            "organizationType": org.type.name,
+            "organization_name": "New Test Organization",
+            "organization_address": "123 New Test St, Springfield, IL 62701",
+            "organization_type": org.type.name,
             "taDepth": test_request.depth.name,
-            "description": test_request.description
+            "description": "A brand new request from a brand new organization"
         }
         response = api_client.post("/api/process-intake-form/", data=data)
         assert response.status_code == status.HTTP_201_CREATED
