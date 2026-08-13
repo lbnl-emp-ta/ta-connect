b3f338efca4367c8ca031fee4b3758827d7c5f47
Cody O'Donnell <ctodonnell@lbl.gov>
2026-08-13 09:17:34 -0700

    Add initial export functionality via the admin

--- backend/api/core/admin.py
+++ backend/api/core/admin.py
@@ -1,12 +1,67 @@
 from django.contrib import admin
 from django.contrib.auth import get_user_model
 from django.contrib.auth.models import Group
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
 from core.models import *
 
 admin.site.unregister(Group)
 
+
+class RequestResource(resources.ModelResource):
+    class Meta:
+        model = Request
+        fields = (
+            'id',
+            'owner',
+            'program',
+            'lab',
+            'expert',
+            'organization',
+            'status',
+            'depth',
+            'description',
+            'challenges',
+            'goals',
+            'effort',
+            'date_created',
+            'proj_start_date',
+            'proj_completion_date',
+            'actual_completion_date',
+            'topics',
+        )
+
+    def dehydrate_owner(self, obj):
+        return str(obj.owner) if obj.owner else ''
+
+    def dehydrate_program(self, obj):
+        return obj.program.name if obj.program else ''
+
+    def dehydrate_lab(self, obj):
+        return obj.lab.name if obj.lab else ''
+
+    def dehydrate_expert(self, obj):
+        return obj.expert.email if obj.expert else ''
+
+    def dehydrate_organization(self, obj):
+        return obj.organization.name if obj.organization else ''
+
+    def dehydrate_status(self, obj):
+        return obj.status.name if obj.status else ''
+
+    def dehydrate_depth(self, obj):
+        return obj.depth.name if obj.depth else ''
+
+    def dehydrate_topics(self, obj):
+        return ', '.join(obj.topics.values_list('name', flat=True))
+
+
+class RequestAdmin(ImportExportModelAdmin):
+    resource_classes = [RequestResource]
+
+
 admin.site.register(get_user_model())
-admin.site.register(Request)
+admin.site.register(Request, RequestAdmin)
 admin.site.register(Customer)
 admin.site.register(CustomerRequestRelationship)
 admin.site.register(Organization)
